3215e599bcd22a0a6ce2268095bdde8de46b41ac
Paul Miller <paul@paulmillr.com>
2021-12-30 14:29:31 +0200

    Try to fix deno

--- mod.ts
+++ mod.ts
@@ -1,8 +1,8 @@
 import { getPublicKey, sign, verify, utils, CURVE, Point, ExtendedPoint } from './index.ts';
-import { SHA512 } from 'https://denopkg.com/chiefbiiko/sha512/mod.ts';
+import { Sha512 } from 'https://deno.land/std@0.119.0/hash/sha512.ts';
 
 utils.sha512 = async (message: Uint8Array): Promise<Uint8Array> => {
-  return new SHA512().update(message).digest() as Uint8Array;
+  return new Uint8Array(new Sha512().update(message).arrayBuffer());
 };
 
 export { getPublicKey, sign, verify, utils, CURVE, Point, ExtendedPoint };
