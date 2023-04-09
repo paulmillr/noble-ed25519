bcea19f261818d260a6310eb422d56dc7dc47a0a
Quentin Adam <quentinadam@gmail.com>
2023-04-09 11:04:39 -0400

    Update mod.ts file to export etc

--- mod.ts
+++ mod.ts
@@ -1,12 +1,6 @@
 // prettier-ignore
-import {
-  CURVE, ExtendedPoint, utils,
+export {
+  CURVE, ExtendedPoint, etc, utils,
   getPublicKey, sign, verify,
   getPublicKeyAsync, signAsync, verifyAsync,
 } from './index.ts';
-// prettier-ignore
-export {
-  CURVE, ExtendedPoint, utils,
-  getPublicKey, sign, verify,
-  getPublicKeyAsync, signAsync, verifyAsync,
-};
