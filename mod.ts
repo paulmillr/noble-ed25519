645fab1125676ad6262b9a0f8916f5b053d30ecf
Quentin Adam <quentinadam@gmail.com>
2023-04-10 10:21:59 -0400

    Revert export {...} from '...'

--- mod.ts
+++ mod.ts
@@ -1,6 +1,12 @@
 // prettier-ignore
-export {
+import {
   CURVE, ExtendedPoint, etc, utils,
   getPublicKey, sign, verify,
   getPublicKeyAsync, signAsync, verifyAsync,
 } from './index.ts';
+// prettier-ignore
+export {
+  CURVE, ExtendedPoint, etc, utils,
+  getPublicKey, sign, verify,
+  getPublicKeyAsync, signAsync, verifyAsync,
+};
