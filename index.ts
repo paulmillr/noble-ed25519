d5fdde943ddea38d0a7711f8567031f3966cd2d4
Paul Miller <paul@paulmillr.com>
2025-07-03 18:26:08 +0000

    Fix formatting

--- index.ts
+++ index.ts
@@ -56,16 +56,16 @@ export type EdwardsOpts = Readonly<{
 
 // ## Helpers
 // ----------
-function safeCaptureStackTrace(...args: Parameters<typeof Error.captureStackTrace>): void {
+const captureTrace = (...args: Parameters<typeof Error.captureStackTrace>): void => {
   if ('captureStackTrace' in Error && typeof Error.captureStackTrace === 'function') {
     Error.captureStackTrace(...args);
   }
-}
-function err(m = ''): never {
-  const e = new Error(m);
-  safeCaptureStackTrace(e, err);
+};
+const err = (message = ''): never => {
+  const e = new Error(message);
+  captureTrace(e, err);
   throw e;
-}
+};
 const isBig = (n: unknown): n is bigint => typeof n === 'bigint'; // is big integer
 const isStr = (s: unknown): s is string => typeof s === 'string'; // is string
 const isBytes = (a: unknown): a is Uint8Array =>
